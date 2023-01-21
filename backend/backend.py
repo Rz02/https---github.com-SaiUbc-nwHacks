0e9129a90a13310655ec046ce3a0dd83600c24df
Rocky Zhao <rockyzhao02@gmail.com>
2023-01-21 14:03:08 -0800

    api try

--- backend/backend.py
+++ backend/backend.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
+import functools
+
 
 app = Flask(__name__)
 
@@ -10,21 +12,57 @@ def index():
 def map():
     return 'MAP PAGE'
 
+# dynamic route: 
+# 
+# @app.route('/product/<int:product_id>')
+# def product(product_id):
+#    product = get_product_by_id(product_id)
+#    return render_template('product.html', product=product)
+
+# @app.route('/user/<string:username>')
+# def user(username):
+#     user = get_user_by_name(username)
+#     return render_template('user.html', user=user)
+
+# @app.route('/search/<search_query>')
+# @app.route('/search/<search_query>/<int:page>')
+# def search(search_query, page=1):
+#     results = search_database(search_query, page)
+#     return render_template('search.html', results=results)
+
+valid_api_keys = {}
+@app.route('/<api_key>/secure_route')
+def secure_route(api_key):
+    if api_key not in valid_api_keys:
+        return jsonify(error = "Invalid API key"), 401
+    return "This is a secure route"
+
 @app.route("/bus")
 def bus():
     return 'BUS PAGE'
 
-@app.route('/bus/<int:id>')
-def bus_num(id):
-    return f"This is the bus {id}"
+@app.route('/bus/<int:bus_id>')
+def bus_num(bus_id):
+    return f"This is the bus {bus_id}"
 
-@app.route("/station")
-def station():
-    return 'STATION PAGE'
+  #  bus = search_database(search_query, page)
+  #  return render_template('**HTML**', bus = bus)
 
-@app.route('/station/<int:id>')
-def station_num(id):
-    return f"This is the station {id}"
+@app.route("/stop")
+def stop():
+    return 'STOP PAGE'
+
+@app.route('/stop/<int:stop_id>')
+def stop_num(stop_id):
+    return f"This is the station {stop_id}"
+
+@app.route("/route")
+def route():
+    return 'ROUTE PAGE'
+
+@app.route('/route/<int:sta_id>')
+def route_num(sta_id):
+    return f"This is the route {sta_id}"
 
 @app.route("/qanda")
 def qanda():
