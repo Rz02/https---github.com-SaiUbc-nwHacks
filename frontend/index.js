9b2f45035e3a1229637a949588690036448941aa
nvk2003 <nvk2003@students.cs.ubc.ca>
2023-01-22 05:18:58 -0800

    Implemented code to save bus stop no. else error

--- frontend/index.js
+++ frontend/index.js
@@ -1,8 +1,21 @@
 var searchButton = document.getElementById("go-btn");
 var searchBar = document.getElementById("input-element");
+var errorMessage = document.getElementById("error-message");
 
-searchButton.addEventListener("click", function() {
-    var busStopNo = searchBar.value;
-    console.log(busStopNo);
-    // you can use the searchQuery variable in your code here
+searchButton.addEventListener("click", search);
+    searchBar.addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            search();
+        }
     });
+
+    function search() {
+        var busStopNo = searchBar.value;
+        if(isNaN(busStopNo)){
+            alert("Please Enter A Valid Bus Stop No.");
+        } else {
+            console.log(busStopNo);
+            alert(busStopNo)
+            // you can use the searchQuery variable in your code here
+        }
+    }
