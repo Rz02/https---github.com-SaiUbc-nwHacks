eb205fe362eba05f092865093bc77287a4c104fc
nvk2003 <nvk2003@students.cs.ubc.ca>
2023-01-22 04:56:44 -0800

    Added new designs and variable to save the input

---
+++ frontend/index.js
@@ -0,0 +1,8 @@
+var searchButton = document.getElementById("go-btn");
+var searchBar = document.getElementById("input-element");
+
+searchButton.addEventListener("click", function() {
+    var busStopNo = searchBar.value;
+    console.log(busStopNo);
+    // you can use the searchQuery variable in your code here
+    });
