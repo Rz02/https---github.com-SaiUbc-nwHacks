2176e9d07f6661d8eff4316e5c24f7c8e7447e13
Rishabh Ghanty <rishabhg260203@gmail.com>
2023-01-22 02:39:21 -0800

    made more changes

--- googleMapsPlatform/maps.py
+++ googleMapsPlatform/maps.py
@@ -1,11 +1,14 @@
 #this file is for the google maps platform integration 
 from flask import Flask, render_template
+from tilapya import *
+api = RTTI('Ik7TSY2bkJRQzeQeH5LT')
 
 app = Flask("mapAPI")
 
-@app.route('/bus_location/<float:lat>/<float:lng>')
-def update_bus_location(lat, lng):
-    return render_template('./frontend/map.html', lat=lat, lng=lng)
+@app.route('/stop_name/<float:lat>/<float:lng>')
+def stopCoords(stop_num):
+    stop = api.stop(stop_num)
+    return render_template('./frontend/map.html', stop.Latitude, stop.Longitude)
 
 if __name__ == 'mapAPI':
     app.run()
